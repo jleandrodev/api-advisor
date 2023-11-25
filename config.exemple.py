2467eeb96e9fd0b3461ad9c3d87b907d386e8cf1
Ozório <jleandro.dev@gmail.com>
2023-11-25 13:26:25 -0300

    config.exemple added

---
+++ config.exemple.py
@@ -0,0 +1,11 @@
+debug = True
+
+USERNAME = 'Your username'
+PASSWORD = 'Your pass'
+SERVER = 'server'
+DB = 'db'
+
+SQLALCHEMY_DATABASE_URI = 'databaseuri'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'your secret'
